168f54a9a8597c96c7dfd66c4c48532fbcacb7a3
Roman <sharamaga@gmail.com>
2020-05-06 18:23:29 +0300

    Update after review

--- homework/Roman/task_04/task_04.py
+++ homework/Roman/task_04/task_04.py
@@ -6,11 +6,23 @@ def count_holes(in_data):
         return 'ERROR'
 
     # Use dictionary to avoid if else
-    d = {'9': 1, '8': 2, '7': 0, '6': 1, '5': 0, '4': 1, '3': 0, '2': 0, '1': 0, '0': 1, '-': 0}
+    d = {
+        '9': 1,
+        '8': 2,
+        '7': 0,
+        '6': 1,
+        '5': 0,
+        '4': 1,
+        '3': 0,
+        '2': 0,
+        '1': 0,
+        '0': 1,
+        '-': 0
+    }
 
     cnt = 0
-    for i in str(in_data):
-        cnt += d[i]
+    for number in str(in_data):
+        cnt += d.get(number)
 
     return cnt
 
