2d945b202d851b33c1ca2fe39179f656f8bbe259
Roman <sharamaga@gmail.com>
2020-06-10 10:30:05 +0300

    Fix after review

--- homework/Roman/task_05/task_05.py
+++ homework/Roman/task_05/task_05.py
@@ -1,20 +1,18 @@
 def sort_odd(in_data):
-    odd_vars = []
     loc_in_data = in_data
 
-    for i in loc_in_data:
-        if int(i) % 2 != 0:
-            odd_vars.append(i)
+    # Get only odd variables
+    odd_vars = [x for x in loc_in_data if x % 2 != 0]
 
     odd_vars.sort()
 
-    cnt = 0
-    j = 0
-    for i in loc_in_data:
-        if int(i) % 2 != 0:
-            loc_in_data[j] = odd_vars[cnt]
-            cnt += 1
-        j += 1
+    loc_in_data = list(map((lambda x: (x % 2 and odd_vars.pop(0) or x)), loc_in_data))
+
+    # Other, possible, not bad implementation:
+    # for j, elem in enumerate(loc_in_data):
+    #     if int(elem) % 2 != 0:
+    #         loc_in_data[j] = odd_vars.pop(0)
 
     return loc_in_data
+
 
